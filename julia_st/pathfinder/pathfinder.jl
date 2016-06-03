edd3565140d829c0a36ccfbce29c919b9fa796ed
Christophe Foket <christophe.foket@ugent.be>
2016-06-03 14:53:32 +0200

    Added single-threaded Julia version of pathfinder benchmark.
    Former-commit-id: cf5a3d46e486402c02f3d53a790446527d677a35

---
+++ julia_st/pathfinder/pathfinder.jl
@@ -0,0 +1,87 @@
+function getenv(var)
+    ccall((:getenv, "libc"), Ptr{UInt8}, (Ptr{UInt8},), var)
+end
+
+function init(args)
+    global rows
+    global cols
+    global data
+    global result
+
+    if length(args) == 2
+        cols = parse(Int, args[1])
+        rows = parse(Int, args[2])
+    else
+        println("Usage: pathfinder width num_of_steps")
+        exit(0)
+    end
+
+    data = Array{Int}(rows, cols)
+    result = Array{Int}(cols)
+
+    rng = MersenneTwister()
+    srand(rng, 7)
+    rand!(rng, data, 0:9)
+
+    for j = 1:cols
+        result[j] = data[1,j]
+    end
+
+    if getenv("OUTPUT") != C_NULL
+        file = open("output.txt", "w")
+        @printf(file, "wall:\n")
+        for i = 1:rows
+            for j = 1:cols
+                @printf(file, "%d ", data[i,j])
+            end
+            @printf(file, "\n")
+        end
+        close(file)
+    end
+end
+
+function main(args)
+    init(args)
+
+    dst = result
+    src = Array{Int}(cols)
+
+    tic()
+
+    for t = 1:rows-1
+        temp = src
+        src = dst
+        dst = temp
+# #pragma omp parallel for private(min)
+        for n = 1:cols
+            min_ = src[n]
+            if n > 1
+                min_ = min(min_, src[n - 1])
+            end
+            if n < cols
+                min_ = min(min_, src[n + 1])
+            end
+            dst[n] = data[t + 1, n] + min_
+        end
+    end
+
+    @printf("timer: %Lu\n", toq())
+
+    if getenv("OUTPUT") != C_NULL
+        file = open("output.txt", "a")
+        @printf(file, "data:\n")
+        for i = 1:cols
+            @printf(file, "%d", data[1,i])
+        end
+        @printf(file, "\n")
+
+        @printf(file, "result:\n");
+        for i = 1:cols
+            @printf(file, "%d ", dst[i])
+        end
+        @printf(file, "\n");
+        close(file)
+    end
+end
+
+main(ARGS)
