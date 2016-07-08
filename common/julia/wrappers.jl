5ea8b964722a3426a0b9552198e7f07562f16ecc
Jonas Maebe <jonas.maebe@elis.ugent.be>
2016-07-08 11:53:08 +0200

    Wrappers for libm's atan and pow functions
    Includes helpers for applying them to arrays
    
    
    Former-commit-id: ab988383462bdedf0b3d831e3387856f980558dc

--- common/julia/wrappers.jl
+++ common/julia/wrappers.jl
@@ -13,4 +13,28 @@ end
 
 function lrand48()
 	ccall((:lrand48, "libc"), Int64, ())
+
+function catan(angle::Float64)
+    ccall((:atan, "libm"), Float64, (Float64, ), angle)
+end
+
+function catan(angle::Array{Float64})
+    ret = similar(angle)
+    for i in eachindex(angle)
+        ret[i] = catan(angle[i])
+    end
+    ret
+end
+
+
+function cpow(base::Float64, exp::Float64)
+    ccall((:pow, "libm"), Float64, (Float64, Float64), base, exp)
+end
+
+function cpow(angle::Array{Float64})
+    ret = similar(angle)
+    for i in eachindex(angle)
+        ret[i] = cpow(angle[i])
+    end
+    ret
 end
