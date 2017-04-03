f61281c0287558e9f293cec79603d47b6510e3c0
Tim Besard <tim.besard@gmail.com>
2017-04-03 13:44:27 +0200

    julia_cuda: fix function names of kernel profiler.

--- common/julia/kernelprofile.jl
+++ common/julia/kernelprofile.jl
@@ -7,37 +7,41 @@ struct Invocation
     Invocation() = new(CuEvent(), CuEvent())
 end
 
-launch(inv::Invocation) = record(inv.start)
-finish(inv::Invocation) = record(inv.stop)
+measure_launch(inv::Invocation) = record(inv.start)
+measure_finish(inv::Invocation) = record(inv.stop)
 
 const kernels = Dict{String,Vector{Invocation}}()
-function launch(id::String)
+function measure_launch(id::String)
     inv = Invocation()
     push!(get!(kernels, id, Invocation[]), inv)
-    launch(inv)
+    measure_launch(inv)
     return inv
 end
 
 macro measure(id::String, expr)
     quote
-        inv = launch($id)
+        inv = measure_launch($id)
         $(esc(expr))
-        finish(inv)
+        measure_finish(inv)
     end
 end
 
 function report()
+    println()
+    info("Kernel profile report for $(length(kernels)) kernels:")
     for (id, invs) in kernels
         times = Float64[]
         for inv in invs
             synchronize(inv.stop)
             push!(times, elapsed(inv.start, inv.stop))
         end
-        shift!(times)
-        info("Kernel launch $id: " *
-             "min $(trunc(Int, 1_000_000*minimum(times))) µs, " *
-             "mean $(trunc(Int, 1_000_000*mean(times))) " *
-             "± $(trunc(Int, 1_000_000*std(times))) µs")
+        if length(times) > 1
+            shift!(times)
+        end
+        info(" - $id (x $(length(invs))): " *
+             "min $(round(Int, 1_000_000*minimum(times))) µs, " *
+             "mean $(round(Int, 1_000_000*mean(times))) " *
+             (length(times)>1 ? "± $(round(Int, 1_000_000*std(times))) µs" : " µs"))
     end
     empty!(kernels)
 end
