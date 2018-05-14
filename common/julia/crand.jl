150537c594527745e10e8ead990b2859d9017acb
Tim Besard <tim.besard@gmail.com>
2018-05-14 14:47:57 +0200

    crand: fix deprecations.

--- common/julia/crand.jl
+++ common/julia/crand.jl
@@ -1,17 +1,18 @@
-import Base.Random: rand, srand
+using Random
+import Random: rand, srand
 
 const RAND_MAX = typemax(Int32)
 
 
-immutable LibcRNG <: AbstractRNG end
+struct LibcRNG <: AbstractRNG end
 
-srand(r::LibcRNG, seed::Integer) = ccall((:srand, "libc"), Void, (Cuint,), seed)
-rand(r::LibcRNG, ::Type{Cint})   = ccall((:rand, "libc"), Cint, ())
+srand(r::LibcRNG, seed::Integer) = ccall(:srand, Cvoid, (Cuint,), seed)
+rand(r::LibcRNG, ::Type{Cint})   = ccall(:rand,  Cint, ())
 rand(r::LibcRNG)                 = rand(r, Cint)
 
 
-immutable LibcRNG48 <: AbstractRNG end
+struct LibcRNG48 <: AbstractRNG end
 
-srand(r::LibcRNG48, seed::Integer) = ccall((:srand48, "libc"), Void, (Clong,), seed)
-rand(r::LibcRNG48, ::Type{Culong}) = ccall((:lrand48, "libc"), Cint, ())
-rand(r::LibcRNG48, ::Type{Clong})  = ccall((:mrand48, "libc"), Cint, ())
+srand(r::LibcRNG48, seed::Integer) = ccall(:srand48, Cvoid, (Clong,), seed)
+rand(r::LibcRNG48, ::Type{Culong}) = ccall(:lrand48, Cint, ())
+rand(r::LibcRNG48, ::Type{Clong})  = ccall(:mrand48, Cint, ())
