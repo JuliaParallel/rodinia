c283530b5bc25b9d82e0cf06ddeb970ea697bfae
Tim Besard <tim.besard@gmail.com>
2018-07-24 17:35:17 +0200

    Simplify analysis.

--- tools/analyze.jl
+++ tools/analyze.jl
@@ -3,7 +3,7 @@
 # include("common.jl")
 
 
-function analyze(host=gethostname())
+function analyze(host=gethostname(), suite="julia_cuda")
     measurements = CSV.read("measurements_$host.dat")
     grouped = summarize(measurements)
     delete!(grouped, [:kernel_invocations, :benchmark_executions])
@@ -15,79 +15,38 @@ function analyze(host=gethostname())
                       dt->DataFrame(target = "#device",
                                     time = sum(dt[:time]))))
 
-    # create summary dataframes with one column per suite
-    ## with timings
-    time_analysis = DataFrame(benchmark=String[], target=String[])
-    for suite in suites
-        time_analysis[Symbol(suite)] = Union{Missing,Measurement{Float64}}[]
-    end
-    ## with ratios
-    ratio_analysis = DataFrame(benchmark=String[], target=String[])
-    for suite in non_baseline
-        ratio_analysis[Symbol(suite)] = Union{Missing,Measurement{Float64}}[]
-    end
+    # XXX: add compilation time to host measurements
+    # grouped[(grouped[:suite].==suite) .& (grouped[:target].=="#host"), :time] +=
+    #     grouped[(grouped[:suite].==suite) .& (grouped[:target].=="#compilation"), :time]
 
-    for benchmark in unique(grouped[:benchmark])
-        # get the measurements for this benchmark
-        benchmark_data = grouped[grouped[:benchmark] .== benchmark, :]
-        for target in unique(benchmark_data[:target])
-            # get the measurements for this target
-            target_data = benchmark_data[benchmark_data[:target] .== target, :]
-
-            timings = Union{Missing,Measurement{Float64}}[]
-            for suite in suites
-                measurement = target_data[target_data[:suite] .== suite, :time]
-                if size(measurement, 1) == 1
-                    push!(timings, first(measurement))
-                else
-                    @assert size(measurement, 1) == 0
-                    push!(timings, missing)
-                end
-            end
-            push!(time_analysis, [benchmark target timings...])
-
-            # TODO: make sure multiple #compilation's are dealt with properly
-
-            # calculate performance ratios against the baseline
-            ratios = Union{Missing,Measurement{Float64}}[]
-            if baseline in target_data[:suite]
-                baseline_data = target_data[target_data[:suite] .== baseline, :]
-                others_data = target_data[target_data[:suite] .!= baseline, :]
-                for suite in non_baseline
-                    ratio = if suite in others_data[:suite]
-                        suite_data = target_data[target_data[:suite] .== suite, :]
-                        suite_data[:time][1] / baseline_data[:time][1]
-                    else
-                        missing
-                    end
-                    push!(ratios, ratio)
-                end
-            else
-                for suite in non_baseline
-                    push!(ratios, missing)
-                end
-            end
-            push!(ratio_analysis, [benchmark target ratios...])
+    # select values for the requested suite, adding ratios against the baseline
+    analysis = by(grouped, [:benchmark, :target]) do df
+        baseline_data = df[df[:suite] .== baseline, :]
+        suite_data = df[df[:suite] .== suite, :]
+        time = if size(suite_data, 1) == 1
+            suite_data[1, :time]
+        else
+            missing
         end
+        ratio = if size(baseline_data,1) == 1 && size(suite_data, 1) == 1
+            suite_data[1, :time] / baseline_data[1, :time]
+        else
+            missing
+        end
+        DataFrame(time = time, ratio = ratio)
     end
 
     # calculate ratio grand totals
-    geomean(x) = prod(x)^(1/length(x))  # analysis contains normalized numbers, so use geomean
-    ranges = filter(target->startswith(target, '#'), unique(ratio_analysis[:target]))
-    for range in ranges
-        times = Union{Missing,Measurement{Float64}}[]
-        for suite in non_baseline
-            total_time = geomean(ratio_analysis[ratio_analysis[:target] .== range, Symbol(suite)])
-            push!(times, total_time)
-        end
-        push!(ratio_analysis, ["#all", range, times...])
+    geomean(x) = prod(x)^(1/length(x))  # ratios are normalized, so use geomean
+    totals = by(filter(row->startswith(row[:target], '#'), analysis), [:target]) do df
+        DataFrame(time = sum(df[:time]), ratio = geomean(df[:ratio]))
     end
+    totals = hcat(DataFrame(benchmark=fill("#all", size(totals,1))), totals)
+    analysis = vcat(analysis, totals)
 
-    @info("Analysis complete",
-          timings           = filter(row->startswith(row[:target], '#'), time_analysis),
-          performance_ratio = filter(row->startswith(row[:target], '#'), ratio_analysis))
-    CSV.write("analysis_time_$host.dat", time_analysis)
-    CSV.write("analysis_ratio_$host.dat", ratio_analysis)
+    @info "Analysis complete"
+    println(filter(row->startswith(row[:target], '#'), analysis))
+    CSV.write("analysis_$(host)_$suite.csv", analysis; header=true)
 
     return
 end
